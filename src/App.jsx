3bf8cc0f54bf994c5780c1171730b6d79886c625
akranthi113 <akranthi113@gmail.com>
2026-06-10 13:02:29 +0530

    Update: 10-06-2026 13:02:29.60

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,7 @@ const PrivacyPolicy = () => (
 
 function App() {
   return (
-    <Router>
+    <Router basename="/HirePinnacle50">
       <AuthProvider>
         <div className="flex flex-col min-h-screen bg-slate-50">
           <Navbar />
